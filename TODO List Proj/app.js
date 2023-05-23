20aaaa9b032bf1477c4399bfb6aaa3b73acd8db7
CAMBAM30 <cmcdondle@gmail.com>
2023-05-23 02:32:33 -0600

    add task to js

--- TODO List Proj/app.js
+++ TODO List Proj/app.js
@@ -1,18 +1,42 @@
-$.ajax({
-    type: 'POST',
-    url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=199',
-    contentType: 'application/json',
-    dataType: 'json',
-    data: JSON.stringify({
-        task: {
-            content: 'Do Something Crazy'
-        }
-    }),
-    success: function (response, textStatus) {
-        console.log(response);
-    },
-    error: function (request, textStatus, errorMessage) {
-        console.log(errorMessage);
+$(document).ready(function () {
+    var getAndDisplayAllTasks = function () {
+        $.ajax({
+            type: 'GET',
+            url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=199',
+            dataType: 'json',
+            success: function (response, textStatus) {
+                response.tasks.forEach(function (task) {
+                    $('#todo-list').append('<p>' + task.content + '<p>');
+                })
+            },
+            error: function (request, textStatus, errorMessage) {
+                console.log(errorMessage);
+            }
+        });
     }
+  var createTask = function () {
+    $.ajax({
+        type: 'POST',
+        url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=199',
+        contentType: 'application/json',
+        dataType: 'json',
+        data: JSON.stringify({
+            task: {
+                content: $('#new-task-content').val()
+            }
+        }),
+        success: function (response,textStatus) {
+            console.log(response);
+        },
+        error: function (request, textStatus, errorMessage) {
+            console.log(errorMessage);
+        }
+    });
+   }
+ $('#create-task').on('submit', function (e) {
+    e.preventDefault();
+    createTask();
+   });
 });
-     
+
+
